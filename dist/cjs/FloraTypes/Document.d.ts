eab7a22a843ed40e214e8cd732aa75986288e3f3
l-monninger <liammmonninger@gmail.com>
2021-11-25 21:11:10 -0800

    Improving document type predicate.

--- dist/cjs/FloraTypes/Document.d.ts
+++ dist/cjs/FloraTypes/Document.d.ts
@@ -1,6 +1,7 @@
+import { values } from "faunadb";
 /**
  * Document type predicate. Chcecks that any object is a document and has matching data.
  * @param Predicate is the predicate used to check the data.
  * @returns
  */
-export declare const $Document: (Predicate?: ((obj: any) => boolean) | undefined) => (obj: any) => boolean;
+export declare const $Document: <T>(Predicate?: ((obj: any) => obj is T) | undefined) => (obj: any) => obj is values.Document<T>;
--- src/FloraTypes/Document.ts
+++ src/FloraTypes/Document.ts
@@ -1,3 +1,4 @@
+import { values } from "faunadb";
 import {If,
     IsDoc, Select} from "faunadb/query";
 
@@ -7,7 +8,7 @@ import {If,
  * @param Predicate is the predicate used to check the data.
  * @returns
  */
-export const $Document = (Predicate ? : (obj : any)=>boolean)=>(obj : any) : boolean=>{
+export const $Document = <T>(Predicate ? : (obj : any)=>obj is T)=>(obj : any) : obj is values.Document<T>=>{
     return Predicate ? If(
         IsDoc(obj),
         Predicate(Select("data", obj)),
